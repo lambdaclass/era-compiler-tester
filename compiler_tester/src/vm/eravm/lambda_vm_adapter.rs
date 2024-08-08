df64f2d6cec81e26d011ed1052157cef0206dd8f
Gianbelinche <39842759+gianbelinche@users.noreply.github.com>
2024-08-08 11:27:56 -0300

    Add new storage implementation

--- compiler_tester/src/vm/eravm/lambda_vm_adapter.rs
+++ compiler_tester/src/vm/eravm/lambda_vm_adapter.rs
@@ -12,9 +12,13 @@ use lambda_vm::state::VMState;
 use lambda_vm::store::initial_decommit;
 use lambda_vm::store::InMemory;
 use lambda_vm::store::Storage;
+use lambda_vm::store::InitialStorageMemory;
+use lambda_vm::store::StateStorage;
+use lambda_vm::store::ContractStorageMemory;
 use lambda_vm::value::TaggedValue;
 use lambda_vm::vm::ExecutionOutput;
 use lambda_vm::EraVM;
+use lambda_vm::store::InitialStorage;
 use std::cell::RefCell;
 use std::rc::Rc;
 use web3::types::H160;
@@ -98,12 +102,11 @@ pub fn run_vm(
 
         lambda_contract_storage.insert(key, bytecode_u256);
     }
+    let initial_storage = InitialStorageMemory{initial_storage: lambda_storage};
+    let storage_before_run = initial_storage.clone();
+    let contract_storage = ContractStorageMemory{contract_storage: lambda_contract_storage};
 
-    let mut storage = InMemory::new(lambda_contract_storage, lambda_storage);
-
-    let initial_storage = storage.clone();
-
-    let initial_program = initial_decommit(&mut storage, entry_address);
+    let initial_program = initial_decommit(&initial_storage, &contract_storage, entry_address);
 
     let context_val = context.unwrap();
 
@@ -138,7 +141,7 @@ pub fn run_vm(
         TaggedValue::new_raw_integer(abi_params.r5_value.unwrap_or_default()),
     );
 
-    let mut era_vm = EraVM::new(vm, Rc::new(RefCell::new(storage)));
+    let mut era_vm = EraVM::new(vm,Rc::new(RefCell::new(initial_storage)),Rc::new(RefCell::new(contract_storage)));
     let result = era_vm.run_program_with_custom_bytecode();
     let events = merge_events(&era_vm.state.events);
     let output = match result {
@@ -165,12 +168,11 @@ pub fn run_vm(
     };
 
     for (key, value) in era_vm
-        .storage
-        .borrow_mut()
-        .get_state_storage()
+        .state_storage
+        .storage_changes
         .into_iter()
     {
-        if initial_storage.storage_read(key.clone())? != Some(value.clone()) {
+        if storage_before_run.storage_read(key.clone())? != Some(value.clone()) {
             let mut bytes: [u8; 32] = [0; 32];
             value.to_big_endian(&mut bytes);
             storage_changes.insert(
